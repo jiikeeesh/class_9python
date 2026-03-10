7980c4dbe69304436ef081f8774235189c733453
jiikeeesh <78836181+jiikeeesh@users.noreply.github.com>
2026-03-10 15:09:16 +0545

    added a file and changes

--- hello.py
+++ hello.py
@@ -1,1 +1,9 @@
-print("Hello Class 9!")
+a = int(input("Enter a number: "))
+b = int(input("Enter second number: " ))
+c = int(input("Enter a third number: "))
+if a > b and a > c:
+    print("The largest number is: ", a)
+elif b > a and b > c:
+    print("The largest number is: ", b)
+else:
+    print("The largest number is: ", c)
